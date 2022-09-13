eb5a6ba7af3ab5e8565c5c24cd08076a82766356
Kerron Hutton <Kerron100@gmail.com>
2022-09-12 23:35:50 -0500

    renamed base test file.

---
+++ src/test/java/com/zs/library/my_health_pass_auth/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zs.library.my_health_pass_auth;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertThat(1 + 1).isEqualTo(2);
+  }
+
+}
