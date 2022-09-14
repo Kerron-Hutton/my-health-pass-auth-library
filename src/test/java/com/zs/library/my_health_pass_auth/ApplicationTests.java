254cf9bef15ee18f45ae49a4ef756f0e4781946f
Kerron Hutton <Kerron100@gmail.com>
2022-09-13 19:36:12 -0500

    added postgres test container for testing its implementation.

--- src/test/java/com/zs/library/my_health_pass_auth/ApplicationTests.java
+++ src/test/java/com/zs/library/my_health_pass_auth/ApplicationTests.java
@@ -2,8 +2,14 @@ package com.zs.library.my_health_pass_auth;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.zs.library.my_health_pass_auth.configuration.annotations.enable_postgres_test_container.PostgresTestContainer;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@PostgresTestContainer
+@ActiveProfiles("test")
+@SpringBootTest(classes = ApplicationTests.class)
 class ApplicationTests {
 
   @Test
